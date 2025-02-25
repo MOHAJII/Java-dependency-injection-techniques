5f1a46639c7fc665c00ae72e583e637bb4e03706
mohammedhaji <hmohammediaji@gmail.com>
2025-02-25 07:11:38 +0000

    ajouter l'injection on utilisons le framwork Spring Boot avec la technique du fichier de configuration XML

--- src/main/java/presentation/PresentationV3.java
+++ src/main/java/presentation/PresentationV3.java
@@ -1,47 +1,25 @@
 package presentation;
 
-import dao.DaoImplV1;
 import dao.IDao;
+import metier.IMetier;
 import metier.MetierImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.lang.reflect.Method;
 import java.util.Scanner;
 
-public class PresentationV2 {
+public class PresentationV3 {
     // Version par instanciation dynamique via le setter et via le constructeur
     public static void main(String[] args) {
-        try {
-            Scanner scanner = new Scanner(new File("dependencies-configuration.txt"));
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
 
-            // L'injection de l'implementation de la class dao a partir d'un fichier extern de dconfiguration
-            String daoClass = scanner.nextLine();
-            Class cDao = Class.forName(daoClass);
-            IDao dao = (IDao) cDao.getConstructor().newInstance();
+        IDao dao = context.getBean(IDao.class);
+        IMetier metier = context.getBean(IMetier.class);
 
-
-            String metiierClass = scanner.nextLine();
-            Class cMetier = Class.forName(metiierClass);
-            MetierImpl metier = (MetierImpl) cMetier.getConstructor(IDao.class).newInstance(dao); // Injection via le constructeur
-
-            //  Injection via le setter
-            //  Method setDao = cMetier.getMethod("setDao", IDao.class);
-            //  setDao.invoke(metier, dao);
-
-
-
-            System.out.println("******************PRESENTATION********************");
-            System.out.println(metier.calculate());
-            System.out.println("**************************************************");
-
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
-        }
-
-
-
-
-
+        System.out.println("******************PRESENTATION********************");
+        System.out.print("          ");
+        System.out.println("                 " + metier.calculate());
+        System.out.println("**************************************************");
     }
 }
